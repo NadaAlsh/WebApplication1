9f2d112fda259ca175d7577d8dd3f53c1871f020
83315 <alshaibaninada@gmail.com>
2024-04-16 14:28:09 +0300

    Add project files.

---
+++ WebApplication1/Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(CustomerData.Customers);
+        }
+
+        public IActionResult Details(int id)
+        {
+         
+            var customer = CustomerData.Customers.First(x => x.Id == id);
+
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+    }
+}
---
+++ WebApplication1/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApplication1.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+
+    public class CustomerData
+    {
+        public static List<Customer> Customers = new List<Customer>
+    {
+        new Customer { Id = 1, FirstName = "Nada", LastName = "Alshaibani", Email = "nada@gmail.com", PhoneNumber = "12345" },
+        new Customer { Id = 2, FirstName = "Aseel", LastName = "Abdulghafour", Email = "aseel@gmail.com", PhoneNumber = "98765" },
+        new Customer { Id = 3, FirstName = "Razan", LastName = "Alshatti", Email = "razan@gmail.com", PhoneNumber = "64336" }
+    };
+    }
+}
