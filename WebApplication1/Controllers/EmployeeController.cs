1b1c54c3a65523debcb1053cc250616da5d9028c
83315 <alshaibaninada@gmail.com>
2024-04-17 08:18:00 +0300

    Added Employee

---
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(EmployeeData.Employees);
+        }
+        public IActionResult Details(int id)
+        {
+
+            var employee = EmployeeData.Employees.First(x => x.EmployeeId == id);
+
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+    }
+}
---
+++ WebApplication1/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+    }
+
+    public class EmployeeData
+    {
+        public static List<Employee> Employees = new List<Employee>
+    {
+        new Employee { EmployeeId = 1, FirstName = "Nada", LastName = "Alshaibani", Email = "nada@gmail.com", Department = "IT" },
+        new Employee { EmployeeId = 2, FirstName = "Aseel", LastName = "Abdulghafour", Email = "aseel@gmail.com", Department = "IT" },
+        new Employee { EmployeeId = 3, FirstName = "Razan", LastName = "Alshatti", Email = "razan@gmail.com", Department = "IT" }
+    };
+    
+}
+}
